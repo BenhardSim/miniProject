444e85b8907f5216a2d46c3cc1ad56e1826dd5ea
SabdielTarigan <112071041+SabdielTarigan@users.noreply.github.com>
2022-10-04 21:43:15 +0700

    Update Login.php

--- login/Login.php
+++ login/Login.php
@@ -1,3 +1,49 @@
+<?php
+
+session_start();
+require_once('db_login.php');
+
+
+if (isset($_POST["submit"])){
+    $valid = TRUE; 
+    
+    $email = test_input($_POST['email']);
+    if ($email == ''){
+        $error_email = "Email harus diisi";
+        $valid = FALSE;
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error_email = "Format email tidak valid";
+        $valid = FALSE;
+    }
+    
+    $password = test_input($_POST['password']);
+    if ($password == ''){
+        $error_password = "Password harus diisi";
+        $valid = FALSE;
+    }
+
+    
+    if ($valid){
+        
+        $query = " SELECT * FROM tb_admin WHERE Email='".$email."' AND Password='".$password."' ";
+        
+        $result = $db->query($query);
+        if (!$result){
+            die ("Could not query the database: <br />". $db->error);
+        } else{
+            if ($result->num_rows>0){ 
+                $_SESSION['username'] = $email;
+                header('Location: search_penduduk.php');
+                exit;
+            }else{ 
+                echo '<span class="error">Combination of username and password are not correct.</span>';
+            }
+        }
+        
+        $db->close();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,18 +67,20 @@
                         <br>
 					</div>
 					<div class="card-body">
-						<form action="process.php" method="post" class="login-email">
+                        <form class="login-email" method="POST" autocomplete="on" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 							<div class="input-group">
 								<label for="email">Email:</label>
-								<input type="text" name="email" class="form-control" id="email" aria-describedby="email" placeholder="Email">
+								<input type="email" class="form-control" id="email" name="email" size="30" placeholder="Email" value="<?php if(isset($email)) echo $email;?>">
+                                <div class="text-danger"><?php if(isset($error_email)) echo $error_email;?></div>
 							</div>
                             <br>
 							<div class="input-group">
 								<label for="password">Password:</label>
-								<input type="password" name="password" class="form-control" id="password" placeholder="Password">
+                                <input type="password" class="form-control" id="password" name="password" size="30" placeholder="Password" value="">
+                                <div class="text-danger"><?php if(isset($error_password)) echo $error_password;?></div>
 							</div>
                             <br>
-							<button type="submit" class="btn btn-primary">Submit</button>
+							<button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
 						</form>
 
 					</div>
@@ -42,4 +90,6 @@
 		</div>
 
 	</div>
-</body>
+
+  </body>
+</html>
